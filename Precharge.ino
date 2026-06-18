f57bc9336d5fd1f25eb70a1f3e5c4268d9bc2a3d
Aaron Birky <abirky@iastate.edu>
2026-06-17 22:28:55 -0400

    Remove voltage comparison
    The board now listens for a precharge completion signal from the CAN shield.
    It also no longer sends the signal to RTD, as that should be handled by the CAN shield.

--- Precharge.ino
+++ Precharge.ino
@@ -2,21 +2,17 @@
 // #define DEBUG
 
 // Pin Assignments
-const int ir_plus                = 3;  // IR+ relay
-const int ir_minus               = 5;  // IR- relay
-const int precharge              = 7;  // Precharge relay
-const int precharge_complete_out = 10; // AUX 1 - Precharge completion signal to RTD
-const int rtd_button_in          = 9;  // RTD enable button
-const int shutdown_in            = 11; // Shutdown signal
-const int complete_led           = 4;  // Precharge complete green LED
-const int fault_led              = 6;  // Fault red LED
-const int tractive_system_in     = A0; // Tractive system voltage in
-const int battery_in             = A1; // Battery voltage in
+#define ir_plus                (3)  // IR+ relay
+#define ir_minus               (5)  // IR- relay
+#define precharge              (7)  // Precharge relay
+#define precharge_complete_in  (10) // AUX 1 - Precharge completion signal from CAN shield
+#define rtd_button_in          (9)  // RTD enable button
+#define shutdown_in            (11) // Shutdown signal
+#define complete_led           (4)  // Precharge complete green LED
+#define fault_led              (6)  // Fault red LED
 
 // Constants
-const float PRECHARGE_COMPLETE_PERCENT = 0.91; // Voltage ratio threshold for completion
-const float MIN_BATTERY_VOLTAGE = 2.0; // Minimum battery voltage in case of read errors
-const int PRECHARGE_TIMEOUT_MS = 6000; // Time to wait before precharge timeout (6 seconds)
+#define PRECHARGE_TIMEOUT_MS (6000) // Time to wait before precharge timeout (6 seconds)
 
 // State
 enum States { IDLE, CHARGING_WAIT, COMPLETE };
@@ -27,7 +23,7 @@ unsigned long precharge_start_time = 0; // The time (ms) the precharge process s
 // Debug
 #ifdef DEBUG
 unsigned long last_debug_print = 0;// Limits the debug print rate
-const int DEBUG_PRINT_DELAY = 100; // Time to wait before printing another debug statement
+#define DEBUG_PRINT_DELAY (100) // Time to wait before printing another debug statement
 #endif
 
 
@@ -40,12 +36,12 @@ void setup() {
   // Configure pins
   pinMode(shutdown_in,            INPUT);
   pinMode(rtd_button_in,          INPUT);
+  pinMode(precharge_complete_in,  INPUT);
   pinMode(ir_plus,                OUTPUT);
   pinMode(ir_minus,               OUTPUT);
   pinMode(precharge,              OUTPUT);
   pinMode(complete_led,           OUTPUT);
   pinMode(fault_led,              OUTPUT);
-  pinMode(precharge_complete_out, OUTPUT);
 
   // Initialize pin state
   digitalWrite(ir_plus, LOW); // Open relay
@@ -53,7 +49,6 @@ void setup() {
   digitalWrite(precharge, LOW); // Open relay
   digitalWrite(fault_led, HIGH); // Precharge incomplete, red light
   digitalWrite(complete_led, LOW); // Precharge incomplete, no green light
-  digitalWrite(precharge_complete_out, LOW); // Precharge incomplete, no signal
 }
 
 
@@ -78,9 +73,6 @@ void loop() {
   // Status LEDs
   digitalWrite(fault_led,    (state == COMPLETE) ? LOW : HIGH);
   digitalWrite(complete_led, (state == COMPLETE) ? HIGH : LOW);
-
-  // Send precharge state to RTD
-  digitalWrite(precharge_complete_out, (state == COMPLETE) ? HIGH : LOW);
 
   switch (state) {
     // Idle - wait for button press
@@ -114,27 +106,9 @@ void loop() {
         state = IDLE;
         break;
       }
-      
-      // Read voltages
-      float tractive_voltage = analogRead(tractive_system_in) * (5.0 / 1023.0);
-      float battery_voltage = analogRead(battery_in) * (5.0 / 1023.0);
-      if (battery_voltage <= MIN_BATTERY_VOLTAGE) {
-#ifdef DEBUG
-        Serial.println("ERROR - Battery voltage too low!");
-#endif
-        state = IDLE;
-        break;
-      }
-      float charge_ratio = tractive_voltage / battery_voltage;
-#ifdef DEBUG
-      if (millis() - last_debug_print >= DEBUG_PRINT_DELAY) {
-        printVoltages(tractive_voltage, battery_voltage, charge_ratio);
-        last_debug_print = millis();
-      }
-#endif
 
       // If charge is sufficient, signal completion
-      if (charge_ratio >= PRECHARGE_COMPLETE_PERCENT) {
+      if (digitalRead(precharge_complete_in) == HIGH) {
 #ifdef DEBUG
         Serial.println("Precharge complete!");
 #endif
